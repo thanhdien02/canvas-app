538dc3f6ab050a97de36c9f37d4689f519e35c2a
thanhdien02 <106503650+thanhdien02@users.noreply.github.com>
2024-08-22 16:17:25 +0700

    initial canvas app

---
+++ src/app/editor/[editerId]/page.tsx
@@ -0,0 +1,7 @@
+import Editor from "@/features/editor/components/editor";
+
+const EditorDetail = () => {
+  return <Editor />;
+};
+
+export default EditorDetail;
---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,13 @@
+interface EditorProps{
+
+}
+const Editor = () => {
+  
+  return ( <>
+    <div className="editor">
+      <h1>Editor</h1>
+    </div>
+  </> );
+}
+ 
+export default Editor;
